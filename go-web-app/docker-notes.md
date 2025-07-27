670e3e81cc5d6eaa102d05f6e2f3cc2dca43a956
Leeban <leeban.xasan@outlook.com>
2025-07-27 13:25:27 +0100

    tweaks to main.go

--- go-web-app/docker-notes.md
+++ go-web-app/docker-notes.md
@@ -1,2 +1,3 @@
 ## Notes - Multi-Stage Docker Builds
 
+'--rm' means remove the container automatically when it stops.
--- go-web-app/main.go
+++ go-web-app/main.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, Distroless")
+	fmt.Println("We are go-liveeeeeee 🎉")
 }
